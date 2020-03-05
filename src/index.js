a729095b6c1792caebebe9f22ba212fa1b3d48fa
Takahashi Noriyuki <takahashi-n@chwd.ch-mk.jp>
2020-03-05 17:24:01 +0900

    Redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Storeの作成
+const store = createStore(
+    combineReducers(reducers),
+    applyMiddleware(logger)
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    // StoreをAppコンポーネントに紐づけ
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
